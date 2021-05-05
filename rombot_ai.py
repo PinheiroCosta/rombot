92db4fc955e6021aa6d337cde42102ee2fa11a24
Rômulo <romulopinheirocosta@gmail.com>
2021-05-05 12:25:28 -0300

    Update rombot_ai.py
    Add polls function
    Add code refactoration

--- rombot_ai.py
+++ rombot_ai.py
@@ -1,5 +1,16 @@
 from rombot_commands import bot
+import ai_library
 import random
+
+
+def speech_option(choice, enunciated):
+  option = {"some": ai_library.soma_lista(enunciated),
+            "liste": enunciated,
+            "diga": " ".join(enunciated),
+            "enquete": ai_library.enquete(enunciated)
+            }
+
+  return option[choice]
 
 
 class Ai:
@@ -10,6 +21,9 @@ class Ai:
     msg = message.content
 
     if message.author == bot.user:
+      if msg.split()[0] == "Enquete:":
+        await message.add_reaction('\N{THUMBS UP SIGN}')
+        await message.add_reaction('\N{THUMBS DOWN SIGN}')
       return
 
     # retorna mensagem caso seja mencionado
@@ -20,16 +34,10 @@ class Ai:
       hello = f"{random.choice(greeting)} {autor.name}, Como posso te ajudar? Converse comigo na forma imperativa, ou digite {bot.command_prefix}help para saber quais comandos posso executar"
       
       try:
-        if ordem == "some":
-          resultado = [int(termo) for termo in enunciado if termo.isnumeric()]
-          resposta = f"O resultado dessa soma é {sum(resultado)}"
-          await message.channel.send(resposta)
-
-        elif ordem == "liste":
-          await message.channel.send(enunciado)
-
-        elif ordem == "imprima":
-          await message.channel.send(" ".join(enunciado))
+        await message.channel.send(speech_option(ordem, enunciado))
 
       except IndexError:
+        await message.channel.send(hello)
+      
+      except KeyError:
         await message.channel.send(hello)
