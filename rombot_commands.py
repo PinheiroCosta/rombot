7f4f9be1cc9322db04e695fa9e1dba4cf8a374a9
Rômulo <romulopinheirocosta@gmail.com>
2021-05-10 17:05:08 -0300

    Update rombot_commands.py
    Add purge function to remove all kind of messages from current channel
    Add clear function to remove only bot messages from current channel

--- rombot_commands.py
+++ rombot_commands.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
+from googleapiclient.discovery import build
 from time import sleep
 import discord
+import os
 
 
 help_command = commands.DefaultHelpCommand(
@@ -26,8 +28,7 @@ async def get_status(ctx, member: discord.Member):
 @bot.command()
 async def bot_gameplay(ctx, *, act):
   """[nome do jogo]* Muda a atividade do bot"""
-  await bot.change_presence(activity=discord.Game(name=act))
-  
+  await bot.change_presence(activity=discord.Game(name=act))  
   
 # Comandos de Gerenciamento  
 @bot.command()
@@ -36,8 +37,17 @@ async def ch_transfer(ctx, member: discord.Member, channel: discord.VoiceChannel
   await member.move_to(channel, reason=None)
   await ctx.channel.send(f"{member} foi para o canal {channel}")
 
-@bot.command()
+@bot.command(name='clear', aliases=['Clear'])
 async def clear(ctx, limit=100):
+  """[número]* Limpa mensagens do bot do canal atual"""
+  await ctx.channel.send(f"Deletando minhas mensagens do canal {ctx.channel.mention} ...")
+  sleep(2)
+  if_not_pinned_and_bot = lambda msg: not msg.pinned and msg.author == bot.user
+  deleted = await ctx.channel.purge(limit=limit, check=if_not_pinned_and_bot)  
+  await ctx.channel.send(f"{len(deleted)} mensagens foram excluídas", delete_after=3)
+
+@bot.command(name='purge', aliases=['Purge'])
+async def clear_all(ctx, limit=100):
   """[número]* Limpa mensagens do canal atual"""
   await ctx.channel.send(f"Deletando mensagens do canal {ctx.channel.mention} ...")
   sleep(2)
@@ -80,3 +90,10 @@ async def unmute(ctx, member: discord.Member):
   """[@membro]* devolve a voz da pessoa"""
   await member.edit(reason=None, mute=0)
   await ctx.channel.send(f"{member.name} já pode falar novamente")
+
+#@bot.command()
+#async def ytsearch(ctx, query: str):
+#    youtube = build("youtube", "v3", developerKey=os.getenv("YOUTUBE_TOKEN"))
+#    search_response = youtube.search().list(q=query, part="snippet", maxResults=5).execute()
+#    await ctx.channel.send(search_response) # It will send the data in a .json format.
+    
